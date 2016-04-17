2a92c90038f71d189fa6ec5804cc0103f4fe6739
Markus Frosch <markus@lazyfrosch.de>
2016-04-17 16:30:54 +0200

    Rewrite container to apache based
    * Update to 3.0.15

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM php:5.6-fpm
+FROM php:5.6-apache
+
+RUN a2enmod rewrite
 
 RUN apt-get update \
  && apt-get install -y --no-install-recommends \
@@ -12,28 +14,24 @@ RUN apt-get update \
       mysql-client \
       patch \
  && rm -rf /var/lib/apt/lists/* \
- && curl -fsL http://pecl.php.net/get/memcache-2.2.7.tgz >> /usr/src/php/ext/memcache.tgz \
- && tar -xf /usr/src/php/ext/memcache.tgz -C /usr/src/php/ext/ \
- && rm /usr/src/php/ext/memcache.tgz \
  && docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr \
  && docker-php-ext-install \
       gd \
       zip \
       mysql \
       pdo_mysql \
-      memcache-2.2.7 \
       mcrypt \
       mbstring \
       json \
       gettext \
+ && pecl install memcache \
+ && docker-php-ext-enable memcache \
  && echo "date.timezone = 'UTC'" > /usr/local/etc/php/php.ini \
  && echo "short_open_tag = 0" >> /usr/local/etc/php/php.ini \
  && curl -sS https://getcomposer.org/installer | php -- --filename=composer --install-dir=/usr/local/bin
 
-ENV INSTALL_PATH=/var/www/html \
-    VIMBADMIN_VERSION=3.0.12
-
-COPY patch /patch
+ENV INSTALL_PATH=/usr/share/vimbadmin \
+    VIMBADMIN_VERSION=3.0.15
 
 RUN cd /tmp \
  && rm -rf $INSTALL_PATH \
@@ -42,16 +40,13 @@ RUN cd /tmp \
  && rm VIMBADMIN.tar.gz \
  && mv ViMbAdmin-${VIMBADMIN_VERSION} $INSTALL_PATH \
  && cd $INSTALL_PATH \
- && composer install \
- && patch $INSTALL_PATH/application/views/mailbox/email/settings.phtml < /patch \
- && rm /patch
+ && chown -R www-data.www-data . \
+ && su -c "composer install" -s /bin/bash www-data
 
 WORKDIR /var/www/html
-VOLUME /var/www/html
-COPY mail.mobileconfig.php /var/www/html/public/mail.mobileconfig.php
-COPY mozilla-autoconfig.xml /var/www/html/public/mail/config-v1.1.xml
+COPY apache.conf /etc/apache2/conf-enabled/vimbadmin.conf
 COPY docker-entrypoint.sh /entrypoint.sh
-COPY application.ini /var/www/html/application/configs/application.ini
+COPY application.ini /usr/share/vimbadmin/application/configs/application.ini
 
 ENTRYPOINT ["/entrypoint.sh"]
-CMD ["php-fpm"]
+CMD ["apache2-foreground"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,43 +1,55 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
-sed -i "s/PRIMARY_HOSTNAME/${HOSTNAME}/g"  /var/www/html/public/mail/config-v1.1.xml
-sed -i "s/PRIMARY_HOSTNAME/${HOSTNAME}/g"  /var/www/html/public/mail.mobileconfig.php
-sed -i "s/UUID2/$(cat /proc/sys/kernel/random/uuid)/g"  /var/www/html/public/mail.mobileconfig.php
-sed -i "s/UUID4/$(cat /proc/sys/kernel/random/uuid)/g"  /var/www/html/public/mail.mobileconfig.php
+#sed -i "s/PRIMARY_HOSTNAME/${HOSTNAME}/g"  /var/www/html/public/mail/config-v1.1.xml
+#sed -i "s/PRIMARY_HOSTNAME/${HOSTNAME}/g"  /var/www/html/public/mail.mobileconfig.php
+#sed -i "s/UUID2/$(cat /proc/sys/kernel/random/uuid)/g"  /var/www/html/public/mail.mobileconfig.php
+#sed -i "s/UUID4/$(cat /proc/sys/kernel/random/uuid)/g"  /var/www/html/public/mail.mobileconfig.php
 
-echo >&2 "Setting Permissions:"
-path='/var/www/html'
-htuser='www-data'
+if [ "$1" = "apache2-foreground" ]; then
+    echo "Setting up configuration..."
+    sed -i "s/PASSWORD/${DB_ENV_MYSQL_PASSWORD}/g" ${INSTALL_PATH}/application/configs/application.ini
+    sed -i "s/HOSTNAME/${HOSTNAME}/g" ${INSTALL_PATH}/application/configs/application.ini
+    sed -i "s/ADMIN_EMAIL/${ADMIN_EMAIL}/g" ${INSTALL_PATH}/application/configs/application.ini
 
-chown -R root:${htuser} ${path}/
-chown -R ${htuser}:${htuser} ${path}/*
+    (
+        echo "resources.auth.oss.rememberme.salt = \"${SALT_REMEMBER}\""
+        echo "defaults.mailbox.password_salt     = \"${SALT_PASSWORD}\""
+    ) >> ${INSTALL_PATH}/application/configs/application.ini
 
-cp ${INSTALL_PATH}/public/.htaccess.dist ${INSTALL_PATH}/public/.htaccess
+    export MYSQL="mysql -uvimbadmin"
+    export MYSQL_HOST="db"
+    export MYSQL_PWD="${DB_ENV_MYSQL_PASSWORD}"
 
-sed -i "s/PASSWORD/${DB_ENV_MYSQL_PASSWORD}/g" ${INSTALL_PATH}/application/configs/application.ini
-sed -i "s/HOSTNAME/${HOSTNAME}/g" ${INSTALL_PATH}/application/configs/application.ini
-sed -i "s/ADMIN_EMAIL/${ADMIN_EMAIL}/g" ${INSTALL_PATH}/application/configs/application.ini
+    connected=0
+    for ((i=0;i<10;i++))
+    do
+        echo "Trying to connect to database..."
+        if $MYSQL -e 'status' >/dev/null; then
+          if [ $($MYSQL -N -s -hdb -e \
+            "select count(*) from information_schema.tables where \
+              table_schema='vimbadmin' and table_name='domain';") -eq 1 ]; then
+            echo "Database seems to be set up."
+            connected=1
+            break
+          else
+            echo "Creating DB and Superuser"
+            HASH_PASS=`php -r "echo password_hash('${ADMIN_PASSWORD}', PASSWORD_DEFAULT);"`
+            ./bin/doctrine2-cli.php orm:schema-tool:create
+            $MYSQL vimbadmin -e \
+              "INSERT INTO admin (username, password, super, active, created, modified) VALUES ('${ADMIN_EMAIL}', '$HASH_PASS', 1, 1, NOW(), NOW())" && \
 
-cat /salts >> ${INSTALL_PATH}/application/configs/application.ini
-
-for ((i=0;i<10;i++))
-do
-    DB_CONNECTABLE=$(mysql -uvimbadmin -p${DB_ENV_MYSQL_PASSWORD} -hdb -P3306 -e 'status' >/dev/null 2>&1; echo "$?")
-    if [[ DB_CONNECTABLE -eq 0 ]]; then
-      if [ $(mysql -N -s -uvimbadmin -p${DB_ENV_MYSQL_PASSWORD} -hdb -e \
-        "select count(*) from information_schema.tables where \
-          table_schema='vimbadmin' and table_name='domain';") -eq 1 ]; then
-        exec "$@"
-      else
-        echo "Creating DB and Superuser"
-        HASH_PASS=`php -r "echo password_hash('${ADMIN_PASSWORD}', PASSWORD_DEFAULT);"`
-        ./bin/doctrine2-cli.php orm:schema-tool:create
-        mysql -u vimbadmin -p${DB_ENV_MYSQL_PASSWORD} -h db vimbadmin -e \
-          "INSERT INTO admin (username, password, super, active, created, modified) VALUES ('${ADMIN_EMAIL}', '$HASH_PASS', 1, 1, NOW(), NOW())" && \
-        echo "Vimbadmin setup completed successfully"
-        exec "$@"
-      fi
+            echo "Vimbadmin setup completed successfully"
+            connected=1
+            break
+          fi
+        fi
+        sleep 5
+    done
+    if [ $connected -ne 1 ]; then
+        echo "Database connection timed out!"
+        exit 1
     fi
-    sleep 5
-done
-exit 1
+
+fi
+
+exec "$@"
